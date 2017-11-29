1fca048ca82248043b29c46005b62cb23eca26f9
stephaniewv <stephaniewv@gmail.com>
2017-11-29 10:34:36 -0500

    Pruebas con JS

--- js/app.js
+++ js/app.js
@@ -1,6 +1,29 @@
+alert('hola')
 /*
  * Funcionalidad de tu producto
  */
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
-console.log(data);
+/*console.log(data);*/
+
+
+google.load("current", {packages:["corechart"]});
+google.setOnLoadCallback(drawChart);
+function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+        ['Task', 'Hours per Day'],
+        ['Work',     11],
+        ['Eat',      2],
+        ['Commute',  2],
+        ['Watch TV', 2],
+        ['Sleep',    7]
+    ]);
+
+    var options = {
+        title: 'My Daily Activities',
+        pieHole: 0.4,
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+    chart.draw(data, options);
+}
